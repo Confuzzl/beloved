6a50a907f97458318509c8b654f1231c55e60537
Confuzzl <fuzzltheduzzl@gmail.com>
2023-12-07 16:40:08 -0500

    maintenance

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const excerpt = document.getElementById("excerpt");
 const annotations = document.getElementById("annotations");
+const focusIndicator = document.getElementById("focus_indicator");
 
 const annotationMap = new Map([
-    [0, `Who is the "she" Beloved is referring to?`],
+  [0, `Who is the "she" Beloved is referring to?`],
 ]);
 
 /**@type {HTMLSpanElement[]}*/
@@ -11,40 +12,53 @@ let selectedPair = null;
 
 /**@param {HTMLSpanElement}poi @param {HTMLDivElement} annotation*/
 function setPair(poi, annotation) {
-    selectedPair?.[0].removeAttribute("selected");
-    selectedPair?.[1].removeAttribute("selected");
-    selectedPair = [poi, annotation];
-    selectedPair[0].setAttribute("selected", "");
-    selectedPair[1].setAttribute("selected", "");
+  selectedPair?.[0].removeAttribute("selected");
+  selectedPair?.[1].removeAttribute("selected");
+  selectedPair = [poi, annotation];
+  selectedPair[0].setAttribute("selected", "");
+  selectedPair[1].setAttribute("selected", "");
 }
 
 /**@param {HTMLSpanElement}poi @param {HTMLDivElement} annotation*/
 function scroll(poi, annotation) {
-    excerpt.scrollTo(0, poi.offsetTop - window.innerHeight / 2);
-    annotations.scrollTo(0, annotation.offsetTop - window.innerHeight / 2);
+  excerpt.scrollTo(0, poi.offsetTop - window.innerHeight / 2);
+  annotations.scrollTo(0, annotation.offsetTop - window.innerHeight / 2);
 }
+
+excerpt.onclick = () => {
+  excerpt.focus();
+  focusIndicator.style.left = 0;
+};
+annotations.onclick = () => {
+  annotations.focus();
+  focusIndicator.style.left = "50%";
+};
+
+excerpt.focus();
 
 let i = 0;
 for (const poi of document.getElementsByClassName("poi")) {
-    poi.id = `poi_${i}`;
-    // const value = annotationMap.get(i);
-    // if (value)
-    const annotation = document.createElement("div");
-    annotation.id = `annotation_${i}`;
-    annotation.classList.add("annotation");
-    annotation.innerText = poi.id;
-    annotation.onclick = () => {
-        setPair(poi, annotation);
-        scroll(poi, annotation);
-        // console.log(poi.innerText);
-    };
-    annotations.appendChild(annotation);
+  poi.id = `poi_${i}`;
+  const poiText = document.createElement("span");
+  [poiText.innerHTML, poi.innerHTML] = [poi.innerHTML, poiText.innerHTML];
+  poi.appendChild(poiText);
 
-    poi.onclick = () => {
-        setPair(poi, annotation);
-        scroll(poi, annotation);
-        // console.log(selectedPair[1]);
-    };
+  // const value = annotationMap.get(i);
+  // if (value)
+  const annotation = document.createElement("div");
+  annotation.id = `annotation_${i}`;
+  annotation.classList.add("annotation");
+  annotation.innerText = poi.id;
 
-    i++;
+  const click = () => {
+    setPair(poi, annotation);
+    // setTimeout(() => {
+    scroll(poi, annotation);
+    // }, 1000);
+  };
+
+  annotation.onclick = click;
+  annotations.appendChild(annotation);
+  poi.onclick = click;
+  i++;
 }
